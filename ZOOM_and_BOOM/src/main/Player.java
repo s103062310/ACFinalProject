839c45d88a66b5c3d4e9246f13790a984dd54bef
Tera <Tera>
2016-05-16 21:46:32 +0800

    add a class for Player

---
+++ ZOOM_and_BOOM/src/main/Player.java
@@ -0,0 +1,33 @@
+package main;
+
+public class Player {
+	private int color;		// a int of random from 0~5	// will be mapped to real color in other class
+	private int score;
+	private String name;
+	public Player(){
+		
+	}
+	public Player(int color , int score , String name){
+		this.color=color;
+		this.score=score;
+		this.name=name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+	public int getColor(){
+		return this.color;
+	}
+	public void setColor(int color){
+		this.color=color;
+	}
+	public int getScore(){
+		return this.score;
+	}
+	public void setScore(int score){
+		this.score=score;
+	}
+}
